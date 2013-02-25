36ea030480befdcaef997c3abc1a8ef1d93a56cc
cube45 <90724@supinfo.com>
2013-02-25 21:26:23 +0100

    translation into english

--- src/main.c
+++ src/main.c
@@ -1,43 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void afficherAide()
+void display_help()
 {
      printf("pc86 v0.1\n\n"
           "Usage:\n"
           "pc86 --help\n"
-          "pc86 [options] fichier\n"
+          "pc86 [options] <file>\n"
           );
 }
 
 int main(int argc, char* argv[])
 {
      int i=0;
-     const char* nomFichier = NULL;
+     const char* file_name = NULL;
      
-     //Parsing des paramètres
+     //Parameters parsing
      for(i=1;i<argc;i++)
-     {//argv[0] contient le nom du programme et argv[1] le premier paramètre
+     {//argv[0] contains the program name and argv[1] the first parameter
           if(strcasecmp("--help", argv[i]) == 0)
           {
-               afficherAide();
+               display_help();
                return EXIT_SUCCESS;
           }
           else if(i == argc-1)
-          {//C'est le dernier paramètre, c'est donc le nom du fichier
-               nomFichier = argv[i];
+          {//It is the last parameter, i.e. the file name
+               file_name = argv[i];
           }
           else
           {
-               printf("Option invalide : %s\n", argv[i]);
-               afficherAide();
+               printf("Invalid option : %s\n", argv[i]);
+               display_help();
                return EXIT_FAILURE;
           }
      }
-     if(nomFichier == NULL)
+     if(file_name == NULL)
      {
-          printf("Nom de fichier requis\n");
-          afficherAide();
+          printf("The file name is required\n");
+          display_help();
           return EXIT_FAILURE;
      }
      return EXIT_SUCCESS;
