024cfc0afa36a32f9f945ef41cbe1e9bbf151819
cube45 <90724@supinfo.com>
2013-07-11 21:45:22 +0200

    utilisation de getopt

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <getopt.h>
 
 void display_help (void)
 {
@@ -13,29 +14,42 @@ void display_help (void)
 
 int main (int argc, char* argv[])
 {
-     int i=0;
      char const * file_name = NULL;
      
      /*Parameters parsing*/
-     for (i=1;i<argc;i++)
-     {/*argv[0] contains the program name and argv[1] the first parameter*/
-          if(strcasecmp ("--help", argv[i]) == 0)
-          {
-               display_help ();
-               return EXIT_SUCCESS;
-          }
-          else if (i == argc-1)
-          {/*It is the last parameter, i.e. the file name*/
-               file_name = argv[i];
-          }
-          else
-          {
-               printf ("Invalid option : %s\n", argv[i]);
-               display_help ();
-               return EXIT_FAILURE;
+     int c;
+     int digit_optind = 0;
+
+     while (1) {
+          int this_option_optind = optind ? optind : 1;
+          int option_index = 0;
+          static struct option long_options[] = {
+               {"help",     no_argument, 0,  'h' },
+               {0,         0,                 0,  0 }
+          };
+
+          c = getopt_long(argc, argv, "h", long_options, &option_index);
+          if (c == -1)
+               break;
+
+          switch (c) {
+               case 'h':
+                    display_help ();
+                    return EXIT_SUCCESS;
+
+               case '?'://An invalid option was supplied
+                    display_help ();
+                    return EXIT_FAILURE;
+               default:
+                    break;
           }
      }
-     if (file_name == NULL)
+
+     if (optind < argc) {
+          file_name = argv[optind++];
+          //TODO:execute this file
+     }
+     else
      {
           printf ("The file name is required\n");
           display_help ();
